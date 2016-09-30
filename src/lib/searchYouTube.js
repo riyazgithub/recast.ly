b600b7c7b342389c0a13023b709bd1faf1bfc94b
riyazgithub <h.a.riyaz@gmail.com>
2016-09-30 15:59:44 -0700

    Completed tests for searchYoutube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,17 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet';
+  url += `&key=${options.key}`;
+  url += `&maxResults=${options.max}`;
+  url += `&q=${options.query}`;
+  url += '&videoEmbeddable=true&type=video';
+
+  $.ajax({
+    url: url,
+    success: function(data) {
+      console.log(data);
+      callback(data.items);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
